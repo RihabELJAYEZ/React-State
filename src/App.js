128f0a7bcfc70ba676d04ede8a353273d53f098f
RihabELJAYEZ <rihabeljayez@gmail.com>
2021-04-19 23:39:19 +0200

    first test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+        fullName:"Rihab EL JAYEZ",
+        profession:"Full-Stack JavaScript student",
+        bio:"My name is Rihab EL JAYEZ and I am a full-stack Web Application Developer student currently living in Monastir, TUNISIA. I have a Bachelor of Science in Computer Science from FSM, and my primary focus and inspiration for my studies is Web Development. ",
+        show:false,
+        image:<img className="photo"  src="myPhoto.jpg"></img>,
+        cont:0
+    }
+  }
+  show=()=> {
+    this.setState({show:!this.state.show})
+  }
+    componentDidMount=() =>{
+    setInterval(()=>{
+  this.setState(prevState=>({cont: prevState.cont +1,}));
+      },1000);
+  };
+  render() {
+    return (
+      <div>
+        
+        {this.state.show &&<div>
+        {this.state.image}
+        </div>}
+        {this.state.show &&<h1>{this.state.fullName}</h1>}
+                {this.state.show &&<h3>{this.state.profession}</h3>}
+                {this.state.show && <p className="bioPage">{this.state.bio}</p>}
+                <button className="buttonCLK" onClick={this.show}>show me</button>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <p className="counter" >Counter:{this.state.cont}</p>
+        
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
